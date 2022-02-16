a9bbefebc01fe295d5d28e05997186bf7ca2ac65
Tushar Diwakar <www.tushardiwakar@gmail.com>
2022-02-16 15:31:44 +0530

    added graphql codegen for converting queries into typescript hooks, fixed CORS error

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -4,34 +4,20 @@ import { Box, Button } from '@chakra-ui/react';
 import Wrapper from '../components/Wrapper';
 import InputField from '../components/InputField';
 import { useMutation } from 'urql';
+import { useRegisterMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
 
-interface registerProps{
-
-}
-
-const REGISTER_MUT = `mutation Register($username: String!, $password: String!){
-  register(options: { username: $username, password: $password }) {
-    errors{
-      field
-      message
-    }
-    
-    user{
-      id
-      createdAt
-      updatedAt
-      username
-    }
-  }
-}
-`
+interface registerProps{}
 
 const Register: React.FC<registerProps> = ({}) => {
-    const [,register] = useMutation(REGISTER_MUT)
+    const [,register] = useRegisterMutation();
     return (
     <Wrapper variant="small">
-    <Formik initialValues={{username: "", password: ""}} onSubmit={(values) => {
-        return register(values);
+    <Formik initialValues={{username: "", password: ""}} onSubmit={async (values,{setErrors}) => {
+        const response = await register(values);
+        if(response.data?.register.errors) {
+          setErrors(toErrorMap(response.data.register.errors));
+        }
     }}>
         {({isSubmitting}) => (
             <Form>
